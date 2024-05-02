772f9a5a846db09da3a834f7b21c6e50b2e4380a
Ana Isabel da Silva Mendes <48804086+anitamnd@users.noreply.github.com>
2024-05-02 12:39:17 +0200

    signal strength less or equal

--- R/blank_filtration.R
+++ R/blank_filtration.R
@@ -1,6 +1,6 @@
 blankFiltration <- function(data, sequence, signalStrength, keepIs) {
   data[sequence[, 1] %in% "Blank"][is.na(data[sequence[, 1] %in% "Blank"])] <- 0
-  bf <- apply(data[sequence[, 1] %in% "Blank"], 1, mean) * signalStrength < 
+  bf <- apply(data[sequence[, 1] %in% "Blank"], 1, mean) * signalStrength <= 
                     apply(data[sequence[, 1] %in% "QC"], 1, mean, na.rm = TRUE)
   if (keepIs) { #TODO
     is <- grepl("\\(IS\\)", toupper(data[sequence[, 1] %in% "Name"][, 1]))
